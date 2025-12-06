a688a91be2fb93978a9cc0d8b3ca6d14349ef15e
Skrepy <703189721@qq.com>
2025-12-06 22:42:28 +0800

    🩺main测试更新

--- main.cpp
+++ main.cpp
@@ -6,9 +6,23 @@
 #include"./header/data/Accommodations.h"
 #include"./header/data/BuildingData.h"
 #include"./header/data/UserData.h"
+#include "header/data/StayLog.h"
 
 int main() {
-    UserData user("123","10","123456","123@qq.com","1","4","4");
-    std::cout << user.getData() << std::endl;
-    user.writeToFile();
+    nlohmann::json data;
+    data["bed_number"] = "4";
+    data["building_number"] = "11";
+    data["building_name"] = "B";
+    data["room_number"] = "312";
+    data["reason"] = "入学";
+    nlohmann::json checkIn;
+    checkIn["time"] = Time().getTime();
+    checkIn["handler"] = "123";
+    checkIn["initiator"] = "456";
+    checkIn["dormitory"] = data;
+    StayLog stayLog;
+    stayLog.addCheckOutRecords(checkIn);
+    stayLog.writeToFile();
+    std::cout << checkIn.dump(4) << std::endl;
+
 }
