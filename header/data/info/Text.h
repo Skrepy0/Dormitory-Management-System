fe108cd17ddef020026c781d17c8c18eaca77828
Skrepy <703189721@qq.com>
2025-12-22 16:07:02 +0800

    🐛修复无法打开语言文件的bug

--- header/data/info/Text.h
+++ header/data/info/Text.h
@@ -1,33 +1,43 @@
 ﻿#pragma once
 #include<iostream>
 #include "../../../source/data/library/json.hpp"
-class Text {
-	friend std::ostream& operator<<(std::ostream& out, Text& text);
-private:
-	std::string path;// json键（需要转化）
-	std::string content;// 内容（显示出来的内容）
-	std::string language;// 语言
-	void init();
-	void translate();// 将path转化成content// to do
-	static bool writeLanguageToFile(std::string lang);
-	nlohmann::json readFromJson();
-public:
-	Text();
-	Text(std::string path);
-	static Text of(std::string content);// 不通过json直接显示content内容 用法：Text::of("刘兴康大帅比");可以获得content为"刘兴康大帅比"的Text对象，不通过path转化
-	
-	std::string getPath();// 获取path
-	std::string getContent();// 获取content
-	void setPath(std::string path);// 设置path的值
-	void setContent(std::string content);// 设置content的值
-	static void setLanguage(std::string lang);// 设置语言，lang的值可为"en_us"（英语）、"zh_cn"(中文)
-	void addColorCode(std::string colorCode);// 为content添加颜色代码
-	Text operator+(Text other);// 重载+运算符，连接两个Text对象的content
-	void eraseChar(int index);// 删除content中指定位置的字符
-	void eraseChar(int begin, int end);// 删除content中指定范围的字符
-	void replaceChar(int index, char c);// 替换content中指定位置的字符
-};
 
+class Text {
+    friend std::ostream &operator<<(std::ostream &out, Text &text);
+
+private:
+    std::string path; // json键（需要转化）
+    std::string content; // 内容（显示出来的内容）
+    std::string language; // 语言
+    void init();
+
+    void translate(); // 将path转化成content// to do
+    static bool writeLanguageToFile(const std::string &lang);
+
+    nlohmann::json readFromJson();
+
+public:
+    Text();
+
+    Text(std::string path);
+
+    static Text of(std::string content);
+
+    // 不通过json直接显示content内容 用法：Text::of("刘兴康大帅比");可以获得content为"刘兴康大帅比"的Text对象，不通过path转化
+
+    std::string getPath(); // 获取path
+    std::string getContent(); // 获取content
+    void setPath(std::string path); // 设置path的值
+    void setContent(std::string content); // 设置content的值
+    static void setLanguage(std::string lang); // 设置语言，lang的值可为"en_us"（英语）、"zh_cn"(中文)
+    void addColorCode(std::string colorCode); // 为content添加颜色代码
+    Text operator+(Text other); // 重载+运算符，连接两个Text对象的content
+    Text &operator=(const Text &other);
+
+    void eraseChar(int index); // 删除content中指定位置的字符
+    void eraseChar(int begin, int end); // 删除content中指定范围的字符
+    void replaceChar(int index, char c); // 替换content中指定位置的字符
+};
 
 
 /*  使用参考：
@@ -41,8 +51,8 @@ public:
 		千万不要在这两个文件里面写注释！
 
 	std::cout << t << std::endl;// 输出内容
-	Text::setLanguage("zh_cn");// 设置语言是中文
-	std::cout << Text::of("刘兴康大帅比").getContent() << std::endl;// 虽然这样写可以不乱码，尽量减少这样写，因为这样写的文本不受语言的影响，中英文都是"刘兴康大帅比"
+	Text::setLanguage("zh_cn");// 设置语言是中文"en_us"
+	std::cout << t.getContent() << std::endl;// 虽然这样写可以不乱码，尽量减少这样写，因为这样写的文本不受语言的影响，中英文都是"刘兴康大帅比"
 	t.setPath("test.test01");
 	std::cout << t <<std::endl;
 */
--- source/data/info/Text.cpp
+++ source/data/info/Text.cpp
@@ -12,175 +12,179 @@ namespace {
 }
 
 void Text::init() {
-	char cwd[1024];
-	if (_getcwd(cwd, sizeof(cwd)) != nullptr) {
-		// 获取当前工作目录成功
-		LAN_PATH = std::string(cwd) + "\\src\\data\\lang\\";
-		FILE_PATH = std::string(cwd) + "\\src\\data\\lang\\language.json";
-	}
-	else {
-		std::cout << "Error: Could not get current working directory" << std::endl;
-	}
-	try {
-		std::ifstream in_file(FILE_PATH);
-		if (!in_file.is_open()) {
-			language = "en_us"; // 默认英语
-			return;
-		}
-		nlohmann::json langData;
-		in_file >> langData;
-		if (langData.contains("current") && langData["current"].is_string()) {
-			language = (std::string)(langData["current"]);
-		} else {
-			language = "en_us";
-		}
-		in_file.close();
-	}
-	catch (...) {
-		language = "en_us";// 默认英语
-	}
+    char cwd[1024];
+    if (_getcwd(cwd, sizeof(cwd)) != nullptr) {
+        std::string frontPath = cwd;
+        frontPath.erase(frontPath.size() - 17, 18);
+        // 获取当前工作目录成功
+        LAN_PATH = frontPath + "data\\lang\\";
+        FILE_PATH = frontPath + "data\\lang\\language.json";
+    } else {
+        std::cout << "Error: Could not get current working directory" << std::endl;
+    }
+    try {
+        std::ifstream in_file(FILE_PATH);
+        if (!in_file.is_open()) {
+            language = "en_us"; // 默认英语
+            return;
+        }
+        nlohmann::json langData;
+        in_file >> langData;
+        if (langData.contains("current") && langData["current"].is_string()) {
+            language = (std::string) (langData["current"]);
+        } else {
+            language = "en_us";
+        }
+        in_file.close();
+    } catch (...) {
+        language = "en_us"; // 默认英语
+    }
 }
-void Text::setContent(std::string content)
-{
-	this->content = content;
+
+void Text::setContent(std::string content) {
+    this->content = content;
 }
 
 void Text::setLanguage(std::string lang) {
-	if (!writeLanguageToFile(lang)) {
-		std::cout << "Error: Could not set language";
-	}
-	
+    if (!writeLanguageToFile(lang)) {
+        std::cout << "Error: Could not set language";
+    }
 }
 
 void Text::addColorCode(std::string colorCode) {
-	content = colorCode + content;
+    content = colorCode + content;
 }
 
-Text Text::operator+( Text other) {
-	Text result;
-	result.setContent(this->getContent() + other.getContent());
-	return result;
+Text Text::operator+(Text other) {
+    Text result;
+    result.setContent(this->getContent() + other.getContent());
+    return result;
+}
+
+Text &Text::operator=(const Text &other) {
+    if (this == &other) {
+        return *this;
+    }
+    this->path = other.path;
+    this->content = other.content;
+    this->language = other.language;
+    return *this;
 }
 
 void Text::eraseChar(int index) {
-	if (index < 0 || index >= content.size()) {
-		return;
-	}
-	content.erase(index, 1);
+    if (index < 0 || index >= content.size()) {
+        return;
+    }
+    content.erase(index, 1);
 }
 
 void Text::eraseChar(int begin, int end) {
-	std::string res;
-	for(int i = 0; i < content.size(); i++) {
-		if (i < begin || i > end) {
-			res += content[i];
-		}
-	}
-	content = res;
+    std::string res;
+    for (int i = 0; i < content.size(); i++) {
+        if (i < begin || i > end) {
+            res += content[i];
+        }
+    }
+    content = res;
 }
 
-void Text::replaceChar(int index, char c){
-	content[index] = c;
+void Text::replaceChar(int index, char c) {
+    content[index] = c;
 }
 
 void Text::translate() {
-	auto data = readFromJson();
-	std::cout << data.dump(4) << std::endl;
-	if (!path.empty() && data.contains(path)) {
-		content = (std::string)(data[path]);
-	}
-	else {
-		content = path;
-	}
-	
+    auto data = readFromJson();
+    if (!path.empty() && data.contains(path)) {
+        content = (std::string) (data[path]);
+    } else {
+        content = path;
+    }
 }
 
-bool Text::writeLanguageToFile(std::string lang) {
-	// 先读取当前文件内容（如果存在），再修改并写回
-	std::ifstream in_file(FILE_PATH);
-	nlohmann::json langData;
-	if (in_file.is_open()) {
-		try {
-			in_file >> langData;
-		} catch (...) {
-			langData = nlohmann::json::object();
-		}
-		in_file.close();
-	} else {
-		// 如果无法打开现有语言文件，先创建一个基础对象
-		langData = nlohmann::json::object();
-	}
+bool Text::writeLanguageToFile(const std::string &lang) {
+    // 先读取当前文件内容（如果存在），再修改并写回
+    std::ifstream in_file(FILE_PATH);
+    nlohmann::json langData;
+    if (in_file.is_open()) {
+        try {
+            in_file >> langData;
+        } catch (...) {
+            langData = nlohmann::json::object();
+        }
+        in_file.close();
+    } else {
+        // 如果无法打开现有语言文件，先创建一个基础对象
+        langData = nlohmann::json::object();
+    }
 
-	std::ofstream out_file(FILE_PATH);
-	if (!out_file.is_open()) {
-		return false;
-	}
-	langData["current"] = lang;
-	out_file << langData.dump(4);
-	out_file.close();
-	return true;
+    std::ofstream out_file(FILE_PATH);
+    if (!out_file.is_open()) {
+        return false;
+    }
+    langData["current"] = lang;
+    out_file << langData.dump(4);
+    out_file.close();
+    return true;
 }
 
 nlohmann::json Text::readFromJson() {
-	std::string langFilePath = LAN_PATH + language + ".json";
-	std::ifstream in_file(langFilePath);
-	nlohmann::json langData;
-	if (!in_file.is_open()) {
-		// 无法打开语言文件，返回空对象
-		return nlohmann::json::object();
-	}
-	try {
-		in_file >> langData;
-	} catch (...) {
-		// 返回空对象以避免抛出
-		in_file.close();
-		return nlohmann::json::object();
-	}
-	in_file.close();
-	return langData;
+    std::string langFilePath = LAN_PATH + language + ".json";
+    std::ifstream in_file(langFilePath);
+    nlohmann::json langData;
+    if (!in_file.is_open()) {
+        // 无法打开语言文件，返回空对象
+        return nlohmann::json::object();
+    }
+    try {
+        in_file >> langData;
+    } catch (...) {
+        // 返回空对象以避免抛出
+        in_file.close();
+        return nlohmann::json::object();
+    }
+    in_file.close();
+    return langData;
 }
 
 Text::Text() {
-	init();
+    init();
 }
 
 Text::Text(std::string path) {
-	init();
-	this->path = path;
-	if (!path.empty()) {
-		translate();// 转换path到value
-	}
-	else {
-		this->content = "";
-	}
+    init();
+    this->path = path;
+    if (!path.empty()) {
+        translate(); // 转换path到value
+    } else {
+        this->content = "";
+    }
 }
 
 Text Text::of(std::string text) {
-	Text t("");
-	t.setContent(text);
-	return t;
+    Text t("");
+    t.setContent(text);
+    return t;
 }
 
 std::string Text::getPath() {
-	return this->path;
+    return this->path;
 }
 
 std::string Text::getContent() {
-	
-	if (!path.empty()) {
-		init();
-		translate();
-	}
-	
-	return this->content;
+    if (!path.empty()) {
+        init();
+        translate();
+    }
+
+    return this->content;
 }
 
 void Text::setPath(std::string path) {
-	this->path = path;
-	translate();
+    this->path = path;
+    translate();
 }
 
-std::ostream& operator<<(std::ostream& out, Text& text) {
-	out << text.getContent();
-	return out;
+std::ostream &operator<<(std::ostream &out, Text &text) {
+    out << text.getContent();
+    return out;
 }
