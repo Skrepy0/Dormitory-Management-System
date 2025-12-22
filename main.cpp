35ff66fcaabed6b396fb6c98cc56215310ee022c
Skrepy <703189721@qq.com>
2025-12-22 16:07:24 +0800

    ✅Menu测试

--- main.cpp
+++ main.cpp
@@ -1,28 +1,19 @@
 ﻿#include<iostream>
 #include<vector>
-#include"./header/menu/widget/Select.h"
 #include"./header/data/info/Text.h"
-#include"./header/menu/Menu.h"
 #include"./header/data/Accommodations.h"
 #include"./header/data/BuildingData.h"
 #include"./header/data/UserData.h"
 #include "header/data/StayLog.h"
+#include "header/data/info/Message.h"
+#include <windows.h>
+
 
 int main() {
-    nlohmann::json data;
-    data["bed_number"] = "4";
-    data["building_number"] = "11";
-    data["building_name"] = "B";
-    data["room_number"] = "312";
-    data["reason"] = "入学";
-    nlohmann::json checkIn;
-    checkIn["time"] = Time().getTime();
-    checkIn["handler"] = "123";
-    checkIn["initiator"] = "456";
-    checkIn["dormitory"] = data;
-    StayLog stayLog;
-    stayLog.addCheckOutRecords(checkIn);
-    stayLog.writeToFile();
-    std::cout << checkIn.dump(4) << std::endl;
-
+    BuildingData building("123", "loa", "001", "114514", "123");
+    building.addDormitory(Dormitory("3", "312", 0, Maintenance(Time(), Time(), "asda", "des", "lis", true, "good", "0x123f5d").getMaintenanceData()));
+    Accommodations acc;
+    acc.addBuildings(building);
+    acc.writeInFile();
+    return 0;
 }
