640788aaf3eaa509fcbb651517e1036ded17b89b
Skrepy <703189721@qq.com>
2026-01-07 10:36:42 +0800

    完善DataHelper工具类

--- header/data/DataHelper.h
+++ header/data/DataHelper.h
@@ -2,6 +2,9 @@
 // Created by Skrepy on 2026/1/7.
 //
 #pragma once
+#include <direct.h>
+#include <fstream>
+
 #include "UserData.h"
 #include "../../source/data/library/json.hpp"
 
@@ -9,7 +12,27 @@ class DataHelper {
 private:
     DataHelper() = default;
 
+
 public:
+    static nlohmann::json getAdminData() {
+        std::string FILE_PATH;
+        char cwd[1024];
+        if (_getcwd(cwd, sizeof(cwd)) != nullptr) {
+            std::string frontPath = cwd;
+            frontPath.erase(frontPath.size() - 17, 18);
+            // 获取当前工作目录成功
+            FILE_PATH = frontPath + "data\\data\\Admin.json";
+        } else {
+            std::cout << "Error: Could not get current working directory" << std::endl;
+        }
+        std::ifstream in_file(FILE_PATH);
+
+        nlohmann::json newData = nlohmann::json::parse(in_file);
+
+        in_file.close();
+        return newData;
+    }
+
     static nlohmann::json getUserList() {
         return UserData::readJson()["user"];
     }
